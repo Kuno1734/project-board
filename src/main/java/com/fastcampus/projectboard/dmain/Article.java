ffb1c83f58df6638b9484d1a4062d635c62443e8
Kuno1734 <dlsgus59@gmail.com>
2023-02-13 20:55:14 +0900

    Article ArticleComment 생성

---
+++ src/main/java/com/fastcampus/projectboard/dmain/Article.java
@@ -0,0 +1,14 @@
+package com.fastcampus.projectboard.dmain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String hashtag;
+
+    private LocalDateTime createAt;
+    private String createBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
